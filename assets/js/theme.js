0be2b6961758326f73effc7aed9660e15b3da751
KEVIN SAVALIYA <142149854+KEVIN-SAVALIYA-09@users.noreply.github.com>
2024-05-29 23:48:09 +0530

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,38 @@
-$(document).ready(function(){
-    $('.carousel').carousel({
-        interval: 3000
-    });
+// toggle Button
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+    const navbarNav = document.getElementById('navbarNav');
+    const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+    navbarNav.style.display = expanded ? 'none' : 'flex';
 });
+
+
+// Feature Slides
+
+let currentSlide = 0;
+    const slides = document.querySelectorAll('.carousel-item');
+    
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.classList.toggle('active', i === index);
+        });
+    }
+    
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+    
+    function prevSlide() {
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    function autoSlide() {
+        setInterval(() => {
+            nextSlide();
+        }, 4000); // 
+    }
+
+    window.onload = autoSlide;
